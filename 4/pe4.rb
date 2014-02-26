ef1e817359e08f7f45b85738eb6ad1f66bb5e572
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-02-26 13:20:10 -0800

    Solved problem 4

--- 4/pe4.rb
+++ 4/pe4.rb
@@ -4,7 +4,7 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 =end
 
-DIGITS=2
+DIGITS=3
 
 def is_palindrome?(num)
 	num = num.to_s
@@ -23,4 +23,26 @@ def maxof(digits)
 	max
 end
 
-puts maxof(DIGITS)
+def palindrome_product()
+	term1 = term2 = maxof(DIGITS)
+	big_pal = 0
+	t1min = 0
+
+	while term2 > t1min do
+		term1.downto(1) do |i|
+			product = i*term2
+			if product > big_pal && is_palindrome?(product)
+				big_pal = product
+				t1min = i
+				break
+			end
+		end
+		term2 -= 1
+	end
+
+
+
+	big_pal
+end
+
+puts palindrome_product()
