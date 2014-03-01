666e72c6b63c4d3b1eded4cb17c7373df0d12367
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-03-01 15:02:10 -0800

    Optimize by enforcing opposite parity on m and n

--- 9/pe9.rb
+++ 9/pe9.rb
@@ -9,7 +9,9 @@ Find the product abc.
 =end
 
 def find_triplets_with_sum_of(sum)
-  find_triplets_upto(sum).each do |triplet|
+  max = Math.sqrt(sum).to_i.next
+  puts max
+  find_triplets_upto(max).each do |triplet|
     if triplet.inject(:+) == sum
       puts "found matching triplet " + triplet.to_s
       puts "product abc = " + triplet.inject(:*).to_s
@@ -18,12 +20,20 @@ def find_triplets_with_sum_of(sum)
   end
 end
 
-# compute all triplets up to max m, where m > n
+# Compute all triplets up to max m, where m > n
+#  m and n are constrained to match Euclid's formula
+#  1. b and c have opposite parities (i.e., both are not even/odd)
+#  2. a, b, and c are coprime (follows from 1)
+#  
+#  NOTE: by enforcing opposite parity this def only finds fully reduced triplets
+#  but greatly reduces the compute time. For example, it will not find [6,8,10]
+#  because it is an unreduced form of [3,4,5].
+#  Comment out the conditional for the shovel operator into triplets to turn off
 def find_triplets_upto(max)
   triplets = []
   (2..max).each do |m|
     (1...m).each do |n|
-      triplets << compute_triplet(m,n)
+      triplets << compute_triplet(m,n) if (m.even? && n.odd?) || (m.odd? && n.even?)
     end
   end
   triplets
@@ -35,15 +45,6 @@ def compute_triplet(m,n)
   b = 2*m*n
   c = (m**2) + (n**2)
   [a,b,c]
-end
-
-# Test if a,b,c are a Pythagorean triplet
-def is_triplet?(a,b,c)
-  condition1 = true if a < b && b < c
-  condition2 = true if a**2 + b**2 == c**2
-
-  return true if condition1 && condition2
-  false
 end
 
 # Fibonacci's method for generating primitive Pythagorean triples
@@ -58,6 +59,5 @@ def fibonacci_method(k)
   c = Math.sqrt(k + b**2).to_i
   puts [a,b,c]
 end
-
 
 find_triplets_with_sum_of(1000)
