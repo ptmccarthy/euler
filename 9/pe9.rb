d29ca4f4b6d859f358ec2b786c07a5527b184e2a
Patrick McCarthy <p.t.mccarthy@gmail.com>
2014-03-01 14:15:01 -0800

    Solved using Euclid's, but not perfectly optimized

--- 9/pe9.rb
+++ 9/pe9.rb
@@ -5,57 +5,59 @@ a^2 + b^2 = c^2
 For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-Find the product abc.	
+Find the product abc. 
 =end
 
-def is_triplet?(a,b,c)
-    condition1 = true if a < b && b < c
-    condition2 = true if a**2 + b**2 == c**2
-
-    return true if condition1 && condition2
-    false
+def find_triplets_with_sum_of(sum)
+  find_triplets_upto(sum).each do |triplet|
+    if triplet.inject(:+) == sum
+      puts "found matching triplet " + triplet.to_s
+      puts "product abc = " + triplet.inject(:*).to_s
+      break
+    end
+  end
 end
 
+# compute all triplets up to max m, where m > n
+def find_triplets_upto(max)
+  triplets = []
+  (2..max).each do |m|
+    (1...m).each do |n|
+      triplets << compute_triplet(m,n)
+    end
+  end
+  triplets
+end
+
+# Given m & n compute triplet using the Euclid equations
 def compute_triplet(m,n)
-    a = n**2 - m**2
-    b = 2*n*m
-    c = n**2 + m**2
-    [a,b,c]
+  a = (m**2) - (n**2)
+  b = 2*m*n
+  c = (m**2) + (n**2)
+  [a,b,c]
+end
+
+# Test if a,b,c are a Pythagorean triplet
+def is_triplet?(a,b,c)
+  condition1 = true if a < b && b < c
+  condition2 = true if a**2 + b**2 == c**2
+
+  return true if condition1 && condition2
+  false
 end
 
 # Fibonacci's method for generating primitive Pythagorean triples
+#    Neat, but not actually useful for this problem but I'm leaving it
+#    here for posterity.
 def fibonacci_method(k)
-    a = Math.sqrt(k).to_i
-    n = (k-1)^2
-    b = ((k+1)/2)-1
-    puts b
-    # k = a^2, c^2 = a^2 + b^2
-    c = Math.sqrt(k + b**2).to_i
-    puts [a,b,c]
+  a = Math.sqrt(k).to_i
+  n = (k-1)^2
+  b = ((k+1)/2)-1
+  puts b
+  # k = a^2, c^2 = a^2 + b^2
+  c = Math.sqrt(k + b**2).to_i
+  puts [a,b,c]
 end
 
-# scale the primitive triplet 3,4,5 by a factor
-def scale_triplet(factor)
-    triplet = [3,4,5]
-    scaled = []
-    triplet.each { |x| scaled << x*factor }
-    scaled
-end
 
-def find_triplets_equal_to(max)
-    m = 1
-    n = 2
-    values = compute_triplet(m,n)
-    until values.inject(:+) >= max do
-        m += 1
-        n += 1
-        values = compute_triplet(m,n)
-    end
-    values
-end
-
-#puts find_triplets_equal_to(1000)
-#puts scale_triplet(20)
-fibonacci_method(169)
-
-
+find_triplets_with_sum_of(1000)
